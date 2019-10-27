dfcd0c301b3ba80316e21c216a0deaa548d8f9b5
Pedro Catalão <pedrocatalao@gmail.com>
2019-10-27 14:56:36 +0000

    KISS as possible :)

---
+++ SimHospital.java
@@ -0,0 +1,111 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class SimHospital {
+
+    private static Map<String, Integer> result = Stream.of(States.values()).collect(Collectors.toMap(e -> e.code, e -> 0));
+
+    enum States {
+        Fever("F"), Healthy("H"), Diabetes("D"), Tuberculosis("T"), Dead("X");
+
+        String code;
+
+        States(String code) {
+            this.code = code;
+        }
+
+        public static States getState(String code) throws Exception {
+            for (States state : States.values()) {
+                if (state.code.toLowerCase().equals(code.toLowerCase()))
+                    return state;
+            }
+
+            throw new Exception ("Invalid subject state: " + code);
+        }
+    }
+
+    enum Drugs {
+        Aspirin("As"), Antibiotic("An"), Insulin("I"), Paracetamol("P");
+
+        String code;
+
+        Drugs(String code) {
+            this.code = code;
+        }
+
+        public static Drugs getDrug(String code) throws Exception {
+            for (Drugs drug : Drugs.values()) {
+                if (drug.code.toLowerCase().equals(code.toLowerCase()))
+                    return drug;
+            }
+
+            throw new Exception ("Invalid drug code: " + code);
+        }
+    }
+
+    static class Subject {
+        private States state;
+
+        Subject(States state) {
+            this.state = state;
+        }
+
+        void takeDrugs(List<Drugs> drugs) {
+
+            if (drugs.containsAll(Arrays.asList(Drugs.Aspirin, Drugs.Paracetamol)))
+                state = States.Dead;
+
+            switch (state) {
+                case Fever:
+                    if (drugs.contains(Drugs.Aspirin) || drugs.contains(Drugs.Paracetamol))
+                        state = States.Healthy;
+                    break;
+
+                case Healthy:
+                    if (drugs.contains(Drugs.Antibiotic) && drugs.contains(Drugs.Insulin))
+                        state = States.Fever;
+                    if (drugs.contains(Drugs.Aspirin) || drugs.contains(Drugs.Paracetamol))
+                        state = States.Healthy;
+                    break;
+
+                case Tuberculosis:
+                    if (drugs.contains(Drugs.Antibiotic))
+                        state = States.Healthy;
+                    break;
+
+                case Diabetes:
+                    if (!drugs.contains(Drugs.Insulin))
+                        state = States.Dead;
+
+                case Dead:
+                    Random r = new Random();
+                    if (r.nextInt(1000000) == 1337) // Flying Spaghetti Monster miracle
+                        state = States.Healthy;
+                    break;
+            }
+
+            result.merge(state.code, 1, Integer::sum);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        List<Drugs> drugs = new ArrayList<>();
+        List<Subject> subjects  = new ArrayList<>();
+
+        if(args.length > 1)
+            for (String drug : args[1].split(","))
+                drugs.add(Drugs.getDrug(drug));
+
+        if(args.length > 0)
+            for (String state : args[0].split(","))
+                subjects.add(new Subject(States.getState(state)));
+
+        subjects.forEach(sbj -> sbj.takeDrugs(drugs));
+
+        System.out.println(
+                result.keySet().stream()
+                .map(key -> key + ":" + result.get(key))
+                .collect(Collectors.joining(",")));
+    }
+}
